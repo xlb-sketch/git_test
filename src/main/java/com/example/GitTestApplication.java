faa04b794fa04023ea4c09acbc2d2dbe7e0027ed
xlb-sketch <130472571+xlb-sketch@users.noreply.github.com>
2023-04-12 20:50:46 +0800

    修改Application的Main文件

--- src/main/java/com/example/GitTestApplication.java
+++ src/main/java/com/example/GitTestApplication.java
@@ -8,6 +8,7 @@ public class GitTestApplication {
 
     public static void main(String[] args) {
         System.out.println(args[0]);
+        System.out.println(args[1]);
         SpringApplication.run(GitTestApplication.class, args);
     }
 
